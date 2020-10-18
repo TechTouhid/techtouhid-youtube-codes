9d1e313bba75dc20b6422de57f51f0f97337fcbd
Touhidur Rahman <touhidalamin123@gmail.com>
2020-10-18 21:25:16 +0600

    Learn Python | 8 Interesting Cool Tricks in Python Programming

---
+++ Learn Python 10 Interesting Python Cool Tricks in Python Programming .py
@@ -0,0 +1,46 @@
+# # Tricks num 1
+# def func():
+#     g = 1
+#     f = 2
+#     return g, f
+# x, y = func()
+#
+# print(x, y)
+
+# # Tricks num 2
+# mylist = ['Tech', 'Touhid', 'Youtube']
+# print(mylist[-2])
+
+# Tricks num 3
+# combine_list = ['Tech', 'Touhid', 'Youtube']
+# print(''.join(combine_list))
+
+# # Tricks num 4
+# x = 1
+# y = 2
+# print('Before swapping')
+# print(x, y)
+#
+# x, y = y, x
+# print('after swapping')
+# print(x, y)
+
+# Tricks num 5
+import sys
+# print(f'My python version number is : {sys.version}')
+
+# # Tricks num 6
+# a = [1, 2, 3]
+# x, y, z = a
+# print(x)
+# print(y)
+# print(z)
+
+# # Tricks num 7
+# import itertools
+# lst = [[1,2], [3, 4], [5, 6]]
+# print(list(itertools.chain.from_iterable(lst)))
+
+# Tricks num 8
+subtract = lambda x, y: x-y
+print(subtract(10, 2))
